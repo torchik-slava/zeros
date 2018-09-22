7cf8296076ce8d99e766d07a86fdf6b4efc508dc
Slava Torchik <torhik_slava@mail.ru>
2018-09-22 21:06:46 +0300

    zeros/solution: it was easy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var zeros = 0;
+  for (var i = 1; number/Math.pow(5,i)>1;i++){
+    zeros+=Math.floor(number/Math.pow(5,i));
+  }
+  return zeros;
 }
